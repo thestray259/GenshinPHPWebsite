f288e9b0c51591b37a3eba71d0cd145caa7ffc72
Marltonthegreat <75112858+Marltonthegreat@users.noreply.github.com>
2023-08-25 13:05:13 -0600

    Reorganized files

--- PageTemplates/Artifact.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "../MyHeader.php";
-$artifact = GetArtifactContent($myDbConn, $_GET["ArtifactID"])
-?>
-
-<?php
-$data = mysqli_fetch_row($artifact);
-echo "Set Name:" . $data[1];
-?>
