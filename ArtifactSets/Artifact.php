a601ad47d85a6700e9e321d16c6a9eab45b2af85
Marltonthegreat <75112858+Marltonthegreat@users.noreply.github.com>
2023-08-25 21:14:44 -0600

    Removed unused code

--- ArtifactSets/Artifact.php
+++ ArtifactSets/Artifact.php
@@ -3,18 +3,6 @@ $myHeader = "Artifact ". $_GET["ArtifactID"];
 include_once "../MyHeader.php";
 $artifact = GetArtifactContent($myDbConn, $_GET["ArtifactID"]);
 $data = mysqli_fetch_row($artifact);
-//Code for Uploading Images
-
-//if (count($_FILES) > 0) {
-//    if (is_uploaded_file($_FILES['userImage']['tmp_name'])) {
-//        $imgData = file_get_contents($_FILES['userImage']['tmp_name']);
-//        $imgType = $_FILES['userImage']['type'];
-//        $sql = "UPDATE ArtifactSets SET Image = ? WHERE ArtifactSetId =" . $data[0];
-//        $statement = $myDbConn->prepare($sql);
-//        $statement->bind_param('b', $imgData);
-//        $current_id = $statement->execute() or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_connect_error());
-//    }
-//}
 ?>
 
 <?php
